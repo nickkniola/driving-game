2ba5424a563d2538c4499f37166293229887bf74
Nick Kniola <nickkniola@me.com>
2020-11-05 15:29:04 -0800

    Altered object to have one property of direction, used for changing class

--- main.js
+++ main.js
@@ -1,32 +1,17 @@
-var direction = {north: false, east: true, south: false, west: false};
+var dirObj = {direction: 'rotate-right'};
 
 var car = document.querySelector('.car');
 document.addEventListener('keydown', changeDirection);
 
 function changeDirection(event) {
   if (event.code === 'ArrowUp') {
-    direction.north = true;
-    direction.east = false;
-    direction.south = false;
-    direction.west = false;
-    car.className = 'car rotate-north';
+    dirObj.direction = 'rotate-north';
   } else if (event.code === 'ArrowRight') {
-    direction.north = false;
-    direction.east = true;
-    direction.south = false;
-    direction.west = false;
-    car.className = 'car rotate-right';
+    dirObj.direction = 'rotate-east';
   } else if (event.code === 'ArrowDown') {
-    direction.north = false;
-    direction.east = false;
-    direction.south = true;
-    direction.west = false;
-    car.className = 'car rotate-south';
+    dirObj.direction = 'rotate-south';
   } else if (event.code === 'ArrowLeft') {
-    direction.north = false;
-    direction.east = false;
-    direction.south = false;
-    direction.west = true;
-    car.className = 'car rotate-left';
+    dirObj.direction = 'rotate-west';
   }
+  car.className = 'car ' + dirObj.direction;
 }
