bf7d9edd53901e96fc7924771f817cdb34868387
Nick Kniola <nickkniola@me.com>
2020-11-05 14:45:23 -0800

    Removed console logs

--- main.js
+++ main.js
@@ -5,16 +5,12 @@ document.addEventListener('keydown', changeDirection);
 
 function changeDirection(event) {
   if (event.code === 'ArrowUp') {
-    console.log('north/up');
     car.className = 'car rotate-north';
   } else if (event.code === 'ArrowRight') {
-    console.log('east/right');
     car.className = 'car rotate-right';
   } else if (event.code === 'ArrowDown') {
-    console.log('south/down');
     car.className = 'car rotate-south';
   } else if (event.code === 'ArrowLeft') {
-    console.log('west/left');
     car.className = 'car rotate-left';
   }
 }
