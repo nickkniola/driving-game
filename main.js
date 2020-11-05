f378ec232f5bc254adeaa5d24577a1539201965b
Nick Kniola <nickkniola@me.com>
2020-11-05 14:43:25 -0800

    Added javascript which rotates the car through css classes

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 var direction = ['north', 'east', 'south', 'west'];
 
+var car = document.querySelector('.car');
 document.addEventListener('keydown', changeDirection);
 
-function changeDirection() {
+function changeDirection(event) {
   if (event.code === 'ArrowUp') {
-    console.log('up');
+    console.log('north/up');
+    car.className = 'car rotate-north';
   } else if (event.code === 'ArrowRight') {
-    console.log('right');
+    console.log('east/right');
+    car.className = 'car rotate-right';
   } else if (event.code === 'ArrowDown') {
-    console.log('down');
+    console.log('south/down');
+    car.className = 'car rotate-south';
   } else if (event.code === 'ArrowLeft') {
-    console.log('left');
+    console.log('west/left');
+    car.className = 'car rotate-left';
   }
 }
