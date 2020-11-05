372142da5d812c0b9d55a7d55a80ffa35a48fefa
Nick Kniola <nickkniola@me.com>
2020-11-05 15:11:57 -0800

    Changed array to object which changes based on current direction

--- main.js
+++ main.js
@@ -1,16 +1,32 @@
-var direction = ['north', 'east', 'south', 'west'];
+var direction = {north: false, east: true, south: false, west: false};
 
 var car = document.querySelector('.car');
 document.addEventListener('keydown', changeDirection);
 
 function changeDirection(event) {
   if (event.code === 'ArrowUp') {
+    direction.north = true;
+    direction.east = false;
+    direction.south = false;
+    direction.west = false;
     car.className = 'car rotate-north';
   } else if (event.code === 'ArrowRight') {
+    direction.north = false;
+    direction.east = true;
+    direction.south = false;
+    direction.west = false;
     car.className = 'car rotate-right';
   } else if (event.code === 'ArrowDown') {
+    direction.north = false;
+    direction.east = false;
+    direction.south = true;
+    direction.west = false;
     car.className = 'car rotate-south';
   } else if (event.code === 'ArrowLeft') {
+    direction.north = false;
+    direction.east = false;
+    direction.south = false;
+    direction.west = true;
     car.className = 'car rotate-left';
   }
 }
